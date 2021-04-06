84e2994b34ee4dd11a869091c876bb4f7a719f75
karawan <banat811@gmail.com>
2021-04-06 11:31:57 +0300

    jhg

---
+++ app/src/main/java/tala/mubarki/talafinalproject17/MainShopsActivity.java
@@ -0,0 +1,14 @@
+package tala.mubarki.talafinalproject17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainShopsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_shops);
+    }
+}
